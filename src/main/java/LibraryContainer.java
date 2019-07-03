703e1607f6f106aa05759e4db1bb940047b444e4
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-07-03 23:10:25 +0300

    correct class main

--- src/main/java/LibraryContainer.java
+++ src/main/java/LibraryContainer.java
@@ -1,51 +1,13 @@
 package main.java;
 
-import java.security.Key;
-import java.util.*;
+import java.time.LocalDate;
+import java.util.Map;
 
 public class LibraryContainer {
 
-    private Integer data;
-    private String book;
+    public static void container(Map<LocalDate, String> map, LocalDate data) {
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LibraryContainer that = (LibraryContainer) o;
-        return Objects.equals(data, that.data) &&
-                Objects.equals(book, that.book);
+        System.out.println(map.getOrDefault(data, "At such a date, the book was not taken"));
+
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(data, book);
-    }
-
-    public void container(Integer data, String book) {
-        this.data = data;
-        this.book = book;
-
-        Map<Integer, String> map = new HashMap<>();
-        map.put(2019 / 05 / 01, "Zapovit");
-        map.put(2019 / 05 / 05, "Kobzar");
-        map.put(2019 / 05 / 07, "Kateryna");
-        map.put(2019 / 05 / 10, "Son");
-        map.put(2019 / 05 / 15, "Haidamaky");
-        map.put(2019 / 05 / 18, "Thought");
-        map.put(2019 / 05 / 19, "Poplar tree");
-
-        for (Integer key : map.keySet()) {
-            System.out.println("Key = " + key);
-        }
-        for (Integer key : map.keySet()){
-            for (String value: map.getOrDefault(book));
-        }
-        System.out.println(map.keySet());
-        System.out.println(map.entrySet());
-
-
-
-
-        }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 package main.java;
 
-import java.security.Key;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,15 +8,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Enter the date in the format, Y/M/D: ");
-        Scanner scanner = new Scanner(System.in);
-        int data = scanner.nextInt();
-        LibraryContainer.container(System.out.println(data));
+        Map<LocalDate, String> map = new HashMap<>();
+        map.put(LocalDate.parse("2019-05-01"), "Zapovit");
+        map.put(LocalDate.parse("2019-05-05"), "Kobzar");
+        map.put(LocalDate.parse("2019-05-07"), "Kateryna");
+        map.put(LocalDate.parse("2019-05-10"), "Son");
+        map.put(LocalDate.parse("2019-05-15"), "Haidamaky");
+        map.put(LocalDate.parse("2019-05-18"), "Thought");
+        map.put(LocalDate.parse("2019-05-19"), "Poplar tree");
 
+        LocalDate data;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the date in the format " + LocalDate.now() + "):");
+        try {
+            data = LocalDate.parse(scanner.next());
+            LibraryContainer.container(map, data);
+        } catch (Exception ex) {
+            System.out.println("Incorrect data!");
+        }
 
         System.out.println(map.keySet());
         System.out.println(map.entrySet());
-
 
     }
 }
